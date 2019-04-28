3e8786577222d9170d46d719347de5ee27a254b7
NabilShahid <nabil110176@gmail.com>
2019-04-28 16:08:48 +0500

    Sign in form animation implementation initial

--- src/components/SignIn/signin.jsx
+++ src/components/SignIn/signin.jsx
@@ -25,7 +25,8 @@ const SignInPage = () => (
 const INITIAL_STATE = {
   email: "",
   password: "",
-  error: null
+  error: null,
+  signUpForm:false
 };
 
 class SignInFormBase extends Component {
@@ -38,7 +39,7 @@ class SignInFormBase extends Component {
   onSubmit = event => {
     const { email, password } = this.state;
     event.preventDefault();
-    console.log(this.props.firebase)
+    console.log(this.props.firebase);
     this.props.firebase.authOps
       .doSignInWithEmailAndPassword(email, password)
       .then(() => {
@@ -48,27 +49,24 @@ class SignInFormBase extends Component {
       .catch(error => {
         this.setState({ error });
       });
-
-    
   };
-  
 
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
-  
-  addUser=()=>{
+
+  addUser = () => {
     this.props.firebase.goalOps.retrieveAllGoals("nabil110176@gmail.com");
-  }
+  };
 
   render() {
-    const { email, password, error } = this.state;
+    const { email, password, error,signUpForm } = this.state;
 
     const isInvalid = password === "" || email === "";
 
     return (
       <React.Fragment>
-        <div id="loginContainer">
+        {/* <div id="loginContainer">
           <Row>
             <Col id="leftCol" span={10} />
             <Col id="rightCol" span={14}>
@@ -96,6 +94,77 @@ class SignInFormBase extends Component {
               <Button id="loginButton" onClick={this.addUser}>Sign In</Button>
             </Col>
           </Row>
+        </div> */}
+        <div className={"container "+(signUpForm?"right-panel-active":"")} id="container">
+          <div className="form-container sign-up-container">
+            
+              <form action="#">
+                <h1>Create Account</h1>
+                <div className="social-container">
+                  <a href="#" className="social">
+                    <i className="fab fa-facebook-f" />
+                  </a>
+                  <a href="#" className="social">
+                    <i className="fab fa-google-plus-g" />
+                  </a>
+                  <a href="#" className="social">
+                    <i className="fab fa-linkedin-in" />
+                  </a>
+                </div>
+                <span>or use your email for registration</span>
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button>Sign Up</button>
+              </form>
+            
+          </div>
+          <div className="form-container sign-in-container">
+           
+              <form action="#">
+                <h1>Sign in</h1>
+                <div className="social-container">
+                  <a href="#" className="social">
+                    <i className="fab fa-facebook-f" />
+                  </a>
+                  <a href="#" className="social">
+                    <i className="fab fa-google-plus-g" />
+                  </a>
+                  <a href="#" className="social">
+                    <i className="fab fa-linkedin-in" />
+                  </a>
+                </div>
+                <span>or use your account</span>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <a href="#">Forgot your password?</a>
+                <button>Sign In</button>
+              </form>
+            
+          </div>
+          <div className="overlay-container">
+            <div className="overlay-container">
+              <div className="overlay">
+                <div className="overlay-panel overlay-left">
+                  <h1>Welcome Back!</h1>
+                  <p>
+                    To keep connected with us please login with your personal
+                    info
+                  </p>
+                  <button className="ghost" id="signIn" onClick={()=>{this.setState({signUpForm:false})}}>
+                    Sign In
+                  </button>
+                </div>
+                <div className="overlay-panel overlay-right">
+                  <h1>Hello, Friend!</h1>
+                  <p>Enter your personal details and start journey with us</p>
+                  <button className="ghost" id="signUp" onClick={()=>{this.setState({signUpForm:true})}}>
+                    Sign Up
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </React.Fragment>
     );
