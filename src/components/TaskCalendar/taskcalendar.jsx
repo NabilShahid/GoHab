93d4a94c215eafdc29445c9321880dd6c158c190
NabilShahid <nabil110176@gmail.com>
2019-06-24 00:46:21 +0500

    Tasks calendar page layout and styling

--- src/components/TaskCalendar/taskcalendar.jsx
+++ src/components/TaskCalendar/taskcalendar.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import CalendarView from "../CalendarView/calendarview";
+import { Select } from "antd";
 import "./taskcalendar.css";
+const Option = Select.Option;
 class TaskCalendar extends Component {
   state = {};
   handleClick = info => {
@@ -14,14 +16,14 @@ class TaskCalendar extends Component {
   };
   getTaskAsEvents() {
     return this.props.tasks
-      .filter(task => typeof task.dueDate=="string")
+      .filter(task => typeof task.dueDate == "string")
       .map(task => {
         let event = {
           title: task.name,
           start: task.startDate.split("T")[0],
           end: task.dueDate.split("T")[0],
-          backgroundColor:"#66bb6a",
-          color:"white"
+          backgroundColor: "#66bb6a",
+          color: "white"
         };
         return event;
       });
@@ -29,10 +31,40 @@ class TaskCalendar extends Component {
   render() {
     return (
       <div id="taskCalendarView">
-        <CalendarView
-          calendarEvents={this.getTaskAsEvents()}
-          calendarEventClick={this.handleClick}
-        />
+        <div className="row" id="taskCalendarFilterDiv" style={{ marginBottom: "22px" }}>
+          <div className="col-md-4">
+            <span className="taskCalendarFilterLabel">Goal: </span>
+            <Select
+              onChange={e => this.changeOrderBy(e)}
+              style={{ width: "80%" }}
+              size="small"
+              value={"dueDate"}
+            >
+              <Option value="dueDate">Due Date</Option>
+              <Option value="importance">Importance</Option>
+              <Option value="alphabetical">Alphabetical</Option>
+            </Select>
+          </div>
+          <div className="col-md-3">
+            <span className="taskCalendarFilterLabel">Status: </span>
+            <Select
+              onChange={e => this.changeOrderBy(e)}
+              style={{ width: "70%" }}
+              size="small"
+              value={"dueDate"}
+            >
+              <Option value="dueDate">Due Date</Option>
+              <Option value="importance">Importance</Option>
+              <Option value="alphabetical">Alphabetical</Option>
+            </Select>
+          </div>
+        </div>
+        <div id="mainTasksCalendarDiv"> 
+          <CalendarView
+            calendarEvents={this.getTaskAsEvents()}
+            calendarEventClick={this.handleClick}
+          />
+        </div>
       </div>
     );
   }
@@ -47,4 +79,7 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps,null)(TaskCalendar);
+export default connect(
+  mapStateToProps,
+  null
+)(TaskCalendar);
