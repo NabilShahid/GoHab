8742746151fa93b311bac4feaec76c3a98425784
NabilShahid <nabil110176@gmail.com>
2020-01-02 10:54:16 +0500

    Mul env

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,8 +1,10 @@
+// import {apiUrl} from "../../envdev";
+import {apiUrl, appUrl} from "../../envprod";
 class Api {
   getNotifications(userEmail, items) {
     return new Promise((resolve, reject) => {
       fetch(
-        `https://us-central1-test-project-1-ba9d5.cloudfunctions.net/api/notifications/${userEmail}/${items}`
+        `${appUrl}/${userEmail}/${items}`
       )
         .then(function(response) {
           response.json().then(res => resolve(res));
--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -1,6 +1,6 @@
 import app from "firebase/app";
-import prodConfig from "../../envprod";
-import devConfig from "../../envdev";
+// import firebaseConfig from "../../envdev";
+import firebaseConfig from "../../envprod";
 import AuthOperations from "../database/authOperations";
 import UserOperations from "../database/userOperations";
 import GoalOperations from "../database/goalOperations";
@@ -9,11 +9,11 @@ import TaskOperations from "../database/taskOperations";
 import "firebase/auth";
 import "firebase/firestore";
 
-const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+ 
 
 class Firebase {
   constructor() {
-    app.initializeApp(config);
+    app.initializeApp(firebaseConfig);
     this.authOps = new AuthOperations(
       app.auth(),
       new app.auth.GoogleAuthProvider(),
